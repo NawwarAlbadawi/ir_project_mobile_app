4e01d69a1ece5ebca743affaef9502d40d1dafc4
NawwarBadawy <nawwar.albadawi@hotmail.com>
2026-06-27 07:07:01 +0300

    remove unnecessary view

--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -4,11 +4,11 @@ import 'package:get/get.dart';
 import '../../../../config/design_config.dart';
 import '../controllers/dashboard_controller.dart';
 import '../../search/views/search_view.dart';
-import '../../index_manager/views/index_manager_view.dart';
 import '../../evaluation/views/evaluation_view.dart';
+
 class DashboardView extends GetView<DashboardController> {
   const DashboardView({super.key});
-  static const _pages = [SearchView(), IndexManagerView(), EvaluationView()];
+  static const _pages = [SearchView(), EvaluationView()];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,10 +30,6 @@ class DashboardView extends GetView<DashboardController> {
             CrystalNavigationBarItem(
               icon: Icons.search_rounded,
               unselectedIcon: Icons.search_rounded,
-            ),
-            CrystalNavigationBarItem(
-              icon: Icons.storage_rounded,
-              unselectedIcon: Icons.storage_rounded,
             ),
             CrystalNavigationBarItem(
               icon: Icons.bar_chart_rounded,
